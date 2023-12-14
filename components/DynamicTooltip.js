b625a9d9b860619b8108704e20e5ecde8a25294d
Vipul Gupta <mail.vipulg@gmail.com>
2023-12-14 20:06:38 +0530

    added required code

--- components/DynamicTooltip.js
+++ components/DynamicTooltip.js
@@ -2,14 +2,15 @@ import { useState, useEffect } from 'react';
 import TooltipPortal from './TooltipPortal';
 
 function DynamicTooltip() {
-
-
- 
-
+  const [tooltipContent, setToolTipContent] = useState("");
+  
+  useEffect(()=>{
+    setToolTipContent("this is a tooltip");
+  },[])
   return (
     <TooltipPortal>
       <div style={{ position: 'absolute', top: '50px', left: '50px', backgroundColor: 'gray', padding: '10px', borderRadius: '5px' }}>
-   
+        {tooltipContent}
       </div>
     </TooltipPortal>
   );
--- components/TooltipPortal.js
+++ components/TooltipPortal.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 const portalRoot = typeof document !== 'undefined' ? document.getElementById('tooltip-portal') : null;
 
 function TooltipPortal({ children }) {
-
+    return (
+        <>
+        {
+            ReactDOM.createPortal(children, portalRoot)
+        }
+        </>
+    )
 }
 
 export default TooltipPortal;
